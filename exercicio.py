806fc8d4acd3a553be9a660a4248830956176a8b
thallisss <142070499+thallisss@users.noreply.github.com>
2023-11-15 13:51:41 -0300

    Add files via upload

---
+++ exercicio.py
@@ -0,0 +1,9 @@
+
+def adicionar_nome_e_tipo_sanguineo():
+    nome = input("Digite o nome do paciente:  ")
+    tipo_sanguineo = input("Digite o tipo sanguineo:  ")
+    pessoa = {
+        "Nome": nome,
+        "Tipo Sanguineo": tipo_sanguineo,
+    }
+    
